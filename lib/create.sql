931eb2240ae4eeb9c57e5bee652d22553e5686cf
clifford <clifford@noukod.com>
2018-11-13 17:36:10 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,15 @@
+CREATE TABLE project (
+  id INTEGER PRIMARY KEY,
+   title TEXT,
+   category TEXT,
+   funding_goal INTEGER,
+   start_date TEXT,
+  end_date TEXT
+
+ );
+
+
+ CREATE TABLE user (
+   id INTEGER PRIMARY KEY,
+   name and an age
+);
