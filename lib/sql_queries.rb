06759a958d24d36b6ca35f73a51aad8944d3b04b
clifford <clifford@noukod.com>
2018-11-13 18:00:01 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -16,7 +16,7 @@ end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
 "SELECT users.name, SUM(pledges.amount)
-  FROM projects
+  FROM users
   JOIN pledges
   ON pledges.project_id = projects.id
   GROUP BY projects.title;"
